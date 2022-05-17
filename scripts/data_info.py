c8c0c06bf4f3f846ebc94be98d15c8085081de75
Abel Mitiku <abelmgetnet@gmail.com>
2022-05-17 16:33:01 +0300

    modified data information methods

--- scripts/data_info.py
+++ scripts/data_info.py
@@ -4,6 +4,10 @@ import numpy as np
 
 
 class dataframeInfo:
+    ###############################################################################
+    # data information extractor
+    ################################################################################
+
     def __init__(self, df: pd.DataFrame):
         """
             Returns a dataframe Info Object with the passed DataFrame Data

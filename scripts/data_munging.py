681bb44e3d22ee33c23a732c00eda866f86b8d82
Abel Mitiku <abelmgetnet@gmail.com>
2022-05-17 16:11:49 +0300

    finished data munging scripts

---
+++ scripts/data_munging.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+
+class dataframeMunging:
+    def __init__(self, df: pd.DataFrame):
+        """
+            Returns a data munging Object with the passed DataFrame Data set as its own DataFrame
+            Parameters
+        """
+        self.df = df
+
+    def find_sort_using_column(self, column: str) -> pd.DataFrame:
+        """
+            Returns the objects DataFrame sorted with the specified column
+        """
+        try:
+            return self.df.sort_values(column)
+        except:
+            print("Failed to sort using the specified column")
+
+    def find_normalized_scale_column(self, column: str) -> pd.DataFrame:
+        """
+            Returns the objects DataFrames column scaled using MinMaxScaler
+            Parameters
+        """
+        try:
+            scale_column_df = pd.DataFrame(self.df[column])
+            scale_column_values = scale_column_df.values
+            print(
+                f'The max and min values of the scaled {column} column are:\n\tmax: {scale_column_df.iloc[:, 0].min()}\n\tmin: {scale_column_df.iloc[:, 0].max()}')
+            min_max_scaler = MinMaxScaler()
+            scaled_values = min_max_scaler.fit_transform(scale_column_values)
+            self.df[column] = scaled_values
+
+            return self.df
+
+        except:
+            print("Failed to scale the column")
+
+    def find_standardize_column(self, column: str) -> pd.DataFrame:
+        """
+            Returns the objects DataFrames column normalized using Normalizer
+            Parameters
+        """
+        try:
+            std_column_df = pd.DataFrame(self.df[column])
+            std_column_values = std_column_df.values
+            standardizer = StandardScaler()
+            normalized_data = standardizer.fit_transform(std_column_values)
+            self.df[column] = normalized_data
+
+            return self.df
+        except:
+            print("Failed to standardize the column")

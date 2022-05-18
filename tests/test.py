fb3480c8ca372e510775c2251c057c1d80fabf18
Amulah <55177259+Amulah-98@users.noreply.github.com>
2022-05-18 13:43:25 +0300

    Update test.py
    added the script module and imported them from my folder

--- tests/test.py
+++ tests/test.py
@@ -4,3 +4,11 @@ import unittest
 import numpy as np
 import pandas as pd
 import pandas.api.types as ptypes
+
+sys.path.insert(0, '../scripts/')
+sys.path.append(os.path.abspath(os.path.join('scripts')))
+
+from data_info import dataframeInfo
+from data_preProcess import data_preProcess
+
+

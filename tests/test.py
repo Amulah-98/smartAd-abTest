5a2f08a086975ef9b4842b7b535aae4cf013151d
Amulah <55177259+Amulah-98@users.noreply.github.com>
2022-05-18 13:53:24 +0300

    added dataframe to test

--- tests/test.py
+++ tests/test.py
@@ -11,4 +11,23 @@ sys.path.append(os.path.abspath(os.path.join('scripts')))
 from data_info import dataframeInfo
 from data_preProcess import data_preProcess
 
+df = pd.DataFrame({'numbers': [2, 4, 6, 7, 9], 'letters': ['a', 'b', 'c', 'd', 'e'],
+                   'floats': [0.2323, -0.23123, np.NaN, np.NaN, 4.3434]})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

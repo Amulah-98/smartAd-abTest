340f56255e532cdd83880fc76a4806dc9512735a
Abel Mitiku <abelmgetnet@gmail.com>
2022-05-23 15:34:49 +0300

    modified train script

--- train.py
+++ train.py
@@ -22,10 +22,10 @@ repo = 'https://github.com/Abel-Blue/smartAd-abTest'
 
 # #Get all train, validate and test sets
 browser_v1 = get_train_validate_test_sets(
-    browserEncoded-v1['chrome-mobile'], predicted_column='response', remove_columns=['auction_id'])
+    browserEncoded-v1['device_make'], predicted_column='response', remove_columns=['auction_id'])
 
 # Train All Models For browser version 1
-with mlflow.start_run(experiment_id=1, run_name="Chrome Mobile"):
+with mlflow.start_run(experiment_id=1, run_name="browserEncoded-v1"):
     # Logistic Regression Model
     logistic = train_logistic_model(
         browserEncoded-v1['train_x'], browserEncoded-v1['train_y'], browserEncoded-v1['val_x'], browserEncoded-v1['val_y'])
